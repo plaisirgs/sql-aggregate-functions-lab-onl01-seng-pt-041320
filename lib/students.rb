ac29da6b14a591b83016cdb74e1c8edcb47e86d7
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-06-16 05:44:14 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,4 +1,4 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks. MIN & MAX 
 
 def highest_student_gpa
 end
