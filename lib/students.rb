cb428c8b376341e74417acbef38aa3ccbe05b6ac
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-06-16 05:49:43 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. MIN & MAX TOTAL AVERAGE 
 
 def highest_student_gpa
-  "SELECT MAX(highest_student_gpa) FROM 
+  "SELECT MAX()
 end
 
 def lowest_student_gpa
